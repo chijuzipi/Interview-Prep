338bca00e194edec36707902bc5167b8aa9ca599
Fang Wang <fangwang@Yus-MacBook-Pro.local>
2016-02-19 17:09:17 -0800

    flit coin completed

--- amazon/flipCoin.py
+++ amazon/flipCoin.py
@@ -4,12 +4,13 @@ flip coin to get the longest streak of 1s
 corner:
 1) nums == None or len(nums) == 0
 2) no 0s in nums
-3) other element aside from 1 and 0
+3) other element aside from 1 and 0: assumption no
+4) k == 0
 
 '''
 
 class Solution(object):
-    def flip(self, nums):
+    def flipOne(self, nums):
         
         temp = [float('-inf') for x in range(len(nums))]
         for i in range(len(nums)):
@@ -20,6 +21,30 @@ class Solution(object):
             return len(nums)
         return out
 
+    def flipK(self, nums, k):
+        if nums == None or len(nums) == 0:
+            return 0
+        s = 0
+        f = 0
+        count = 0
+        maxi = 0
+        while f < len(nums):
+            while f < len(nums) and count <= k:
+                maxi = max(maxi,  f-s)
+                if nums[f] == 0:
+                    count += 1
+                f += 1
+
+            if count <= k:
+                maxi = max(maxi,  f-s)
+
+            while s < len(nums) and s <= f and count > k:
+                if nums[s] == 0:
+                    count -= 1
+                s += 1
+
+        return maxi
+            
 def scan(nums, i):
     l = i-1
     r = i+1
@@ -33,7 +58,8 @@ def scan(nums, i):
     return count
 
 if __name__ == "__main__":
-    nums = [1]
-    print Solution().flip(nums)
+    nums = [1,1,0,0,1,1,1,0,1,1,0,1]
+    print Solution().flipOne(nums)
+    print Solution().flipK(nums, 2)
 
         
