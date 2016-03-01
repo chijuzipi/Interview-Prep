43ea7ba4c1ca8b38605a693f1710ef3416e4da85
Fang Wang <fangwang@Yus-MacBook-Pro.local>
2016-03-01 09:57:42 -0800

    tree path sum to target

--- groupon/binaryTreeSumToTarget.py
+++ groupon/binaryTreeSumToTarget.py
@@ -1,1 +1,17 @@
-def find(
+def find(root, target):
+    out = []
+    helper(root, target, out)
+    return out
+
+def helper(root, target, out):
+    if root.val == target:
+        out.append([root])
+
+    map = {}
+    left  = helper(root.left, target, out)
+    right = helper(root.right, target, out)
+    for l in left.keys():
+        for r in right.keys():
+            if root.val + l + r == target:
+                buildPath(left[l], root.val, right)
+
