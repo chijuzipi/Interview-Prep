78d4212d24a90fe60ed1f01f7e1a44217b1f5f0f
chijuzipi <yuzhoubrother@gmail.com>
2016-03-18 23:48:40 -0700

    first repeating character from stream

--- amazon/boundaryTranverse.py
+++ amazon/boundaryTranverse.py
@@ -2,7 +2,11 @@
 Given a binary tree, print boundary nodes of the binary tree Anti-Clockwise starting from the root.
 
 -->
-Transverse Twice: first for left and leaf, second for right
+1) print root
+2) print root.left (only print left node, not the last one)
+3) print root.left (only print leaf node)
+4) print root.right(only print leaf node)
+5) print root.right(only print rightnode, not the last one)
 
 --> 
 corner case:
@@ -10,6 +14,55 @@ corner case:
 
 '''
 
-def boundary(root)
+def boundary(root):
+    if root:
+        print (root.val)
+    else:
+        return 
+        
+    node = root
+    while node.left and node.left.left:
+        node = node.left
+        print node.val
+    printleaf(root.left, 1)
+    printleaf(root.right, -1)
+    printright(root.right)
 
+def printright(node):
+    if not node:
+        return 
+    if node.right:
+        printright(node.right)
+    if node.left == None and node.right == None:
+        return 
+    print node.val
+
+def printleaf(node, order):
+    if not node:
+        return
+    if node.left == None and node.right == None:
+        print node.val
+        return 
+    if order == 1:
+        printleaf(node.left, 1)
+        printleaf(node.right, 1)
+    else:
+        printleaf(node.right, -1)
+        printleaf(node.left, -1)
+
+class Node(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+root = Node(20)
+root.left = Node(8)
+root.right = Node(22)
+root.left.left = Node(4)
+root.left.right = Node(12)
+root.left.right.left = Node(10)
+root.left.right.right = Node(14)
+root.right.right = Node(25)
+boundary(root)
 
--- amazon/firstOccuranceRepeating.py
+++ amazon/firstOccuranceRepeating.py
@@ -1,57 +1,77 @@
 '''
-given an unsorted array with a group of charcters, fin dthe first occurance of non-repeating character.
+The question divided into 2 levels:
+
+1) given an unsorted array with a group of charcters, find the first occurance of non-repeating character
+
+2) Find the first non-repeating character from a stream of characters Given a stream of characters, find the first non-repeating character from stream. You need to tell the first non-repeating character in O(1) time at any moment.
 
 -->
+
 corner case:
 1) all show once
 2) all repeating
 
 '''
 
-def find(s):
+def find1(strs):
     map = {}
-    root = Node(0)
-    curr = root
-    for i in range(len(s)):
-        char = s[i]
-        if char in map:
-            #print 'here', char
-            node = map[char]
-            removeNode(node, curr)
+    for c in strs:
+        if c in map:
+            map[c] += 1
         else:
-            node = Node(char)
-            map[char] = node
-            curr.right = node
-            node.left = curr
-            curr = curr.right
+            map[c] = 1
+    for c in strs:
+        if map[c] == 1:
+            return c
+        
+def find2(stream):
+    root = Node(0) 
+    tail = Node(0)
+    root.right = tail
+    tail.left  = root
+    curr = root
+    map = {}
+    def get():
+        right = root.right
+        if right == tail:
+            return None
+        return right
 
-    return root.right.val
+    def remove(node):
+        if not node:
+            return 
+        left = node.left
+        right= node.right
+        left.right = right
+        right.left = left
+
+    for c in stream:
+        if c in map:
+            remove(map[c])
+            map[c] = None
+        else:
+            node = Node(c)
+            left = tail.left
+            left.right = node
+            node.left = left
+            node.right = tail
+            tail.left = node
+            map[c] = node
+        printNode(root)
 
 def printNode(root):
-    curr = root.right
-    while curr:
-        print curr.val
-        curr = curr.right
-    
-def removeNode(node, curr):
-    if node.left == None:
-        return 
-    if node != curr:
-        node.left.right = node.right
-        node.right.left = node.left
-        node.right = None
-        node.left  = None
-    else:
-        curr = node.left
-        node.left.right = None
-        node.left = None
+    print 
+    while root:
+        print root.x, 
+        root = root.right
 
 class Node(object):
     def __init__(self, x):
-        self.val   = x
-        self.left  = None
-        self.right = None
-    
+        self.x = x
+        self.left = None
+        self.right= None
 
-print find('zabasd')
+find2('abadbf') 
+find2('aabb') 
+
 
